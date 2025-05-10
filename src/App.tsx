70ddb730a84f7f46932e61ed1e002ce06b60e40b
Somadina <kingswatter@gmail.com>
2025-05-10 17:58:31 +0100

    color system

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
 	return (
 		<div>
-			<p className="text-2xl text-red font-semibold">Hello Umu Uwa</p>
+			<p className="text-2xl text-green-70 font-semibold">Hello Umu Uwa</p>
 		</div>
 	);
 }
